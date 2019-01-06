8915498e308858dd75b0d035b1207142c4e9409f
sagarhani <sagarhani33@gmail.com>
2019-01-06 15:53:35 +0530

    Fetching data from API using redux-thunk

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions/index';
+import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
     componentDidMount() {
@@ -8,8 +8,15 @@ class PostList extends React.Component {
     }
 
     render() {
-        return <div> Post List</div>
+        console.log(this.props.posts);
+        return <div>Post List</div>
     };
 };
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
+import postsReducer from './postsReducers';
+
 
 export default combineReducers({
-    replaceMe: () => "hello"
+    posts: postsReducer
 });
